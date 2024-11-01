e3b572212e2d16e6a96ceaf8aac6bd7836291858
granolaa Asus TUF <apple.player4401@gmail.com>
2024-11-01 23:06:25 +0300

    some task b doings

--- hw_2_b/hw_2_b.go
+++ hw_2_b/hw_2_b.go
@@ -2,13 +2,38 @@ package main
 
 import "fmt"
 
-// func solution(nums []int) {
-// 	sum := make([]int, len(nums)+1)
-// 	for i := 1; i < len(nums)+1; i++ {
-// 		sum[i] = sum[i-1] + nums[i-1]
-// 	}
-// 	fmt.Println(sum[1:])
-// }
+func solution(nums []int) {
+	sum := make([]int, len(nums)+1)
+	for i := 1; i < len(nums)+1; i++ {
+		sum[i] = sum[i-1] + nums[i-1]
+	}
+	fmt.Println(sum)
+}
+
+func countprefixsums(nums []int) {
+	prefixsumbyvalue := make(map[int]int)
+	prefixsumbyvalue[0] = 1
+	nowsum := 0
+	for _, val := range nums {
+		nowsum += val
+
+		_, ok := prefixsumbyvalue[nowsum]
+		if !ok {
+			prefixsumbyvalue[nowsum] = 0
+		}
+		prefixsumbyvalue[nowsum] += 1
+	}
+	countneededsumranges(prefixsumbyvalue)
+}
+
+func countneededsumranges(prefixsumbyvalue map[int]int) {
+	cntranges := 0
+	for i := range prefixsumbyvalue {
+		cntsum := prefixsumbyvalue[i]
+		cntranges += cntsum * (cntsum - 1) / 2
+	}
+	fmt.Println(cntranges)
+}
 
 func main() {
 	nK := make([]int, 2)
@@ -20,6 +45,7 @@ func main() {
 	for i := range n {
 		fmt.Scan(&nums[i])
 	}
-	fmt.Println(nK)
-	fmt.Println(nums)
+	// fmt.Println(nK)
+	// fmt.Println(nums)
+	countprefixsums(nums)
 }
